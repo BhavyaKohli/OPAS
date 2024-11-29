4fdf3b1bd9f30f840de6c8b087aa999025e8099f
Bhavya Kohli <bhavyakohli2024@gmail.com>
2024-11-29 11:40:41 +0530

    added matching and lsa to utils

--- opas/utils.py
+++ opas/utils.py
@@ -61,3 +61,22 @@ def gumbel_sinkhorn(log_alpha: torch.Tensor, tau: float = 1.0, n_iter: int = 20,
     else : log_alpha /= tau
     sampled_perm_mat = log_sinkhorn_norm(log_alpha, n_iter)
     return sampled_perm_mat
+
+from scipy.optimize import linear_sum_assignment
+from scipy.sparse import coo_matrix
+
+def gen_assignment(cost_matrix):
+    row, col = linear_sum_assignment(cost_matrix)
+    np_assignment_matrix = coo_matrix((np.ones_like(row), (row, col))).toarray()
+    return np_assignment_matrix
+
+def gumbel_matching(log_alpha : torch.Tensor, noise: bool = True) -> (torch.Tensor,):
+    if noise:
+        uniform_noise = torch.rand_like(log_alpha)
+        gumbel_noise = -torch.log(-torch.log(uniform_noise+1e-20)+1e-20)
+        log_alpha = (log_alpha + gumbel_noise)
+    np_log_alpha = log_alpha.detach().to("cpu").numpy()
+    np_assignment_matrices = [gen_assignment(-x) for x in np_log_alpha]
+    np_assignment_matrices = np.stack(np_assignment_matrices, 0)
+    assignment_matrices = torch.from_numpy(np_assignment_matrices).float().to(log_alpha.device)
+    return assignment_matrices
