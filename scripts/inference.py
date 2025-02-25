c4ece17ad3e8c5151b0b7d67cfe28d99737401d8
Bhavya Kohli <bhavyakohli2024@gmail.com>
2025-02-25 12:30:16 +0530

    moved to argparse positional args, optional switch to use "early" version

--- scripts/inference.py
+++ scripts/inference.py
@@ -1,15 +1,17 @@
 import sys, os
-import subprocess
+import argparse
 
 if __name__ == "__main__":
-    dataset = sys.argv[1]
-    device = sys.argv[2]
-    early = sys.argv[3]
-    expt_ids = sys.argv[4:]
+    parser = argparse.ArgumentParser()
+    parser.add_argument("dataset", type=str)
+    parser.add_argument("device", type=str)
+    parser.add_argument("expt_ids", type=str, nargs="+")
+    parser.add_argument("--early", action="store_true")
+    args = parser.parse_args()
 
-    script = "run_inference_early.py" if early else "run_inference.py"
+    script = "run_inference_early.py" if args.early else "run_inference.py"
 
-    for expt_id in expt_ids:
-        ret = os.system(f"python {script} --device {device} --expt_id {expt_id} --dataset {dataset}")
+    for expt_id in args.expt_ids:
+        ret = os.system(f"python {script} --device {args.device} --expt_id {expt_id} --dataset {args.dataset}")
         if ret == 2:
             break
