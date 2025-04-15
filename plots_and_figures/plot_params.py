a3233c029cea75b41e0cf886dd21993a01aa124a
Bhavya Kohli <bhavyakohli2024@gmail.com>
2025-04-15 23:55:08 +0530

    plots backup

---
+++ plots_and_figures/plot_params.py
@@ -0,0 +1,17 @@
+
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+from matplotlib import font_manager
+
+font = font_manager.FontEntry(fname="arial.ttf", name="Arial")
+font_manager.fontManager.ttflist.append(font)
+
+mpl.rcParams['font.family'] = "Arial"
+
+plot_markersize = 10
+opas_markersize = 250
+opas_idmarkersize = 200
+axis_fontsize = 18
+title_fontsize = 20
+label_fontsize = 12
+legend_fontsize = 15
