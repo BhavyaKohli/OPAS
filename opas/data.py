0e5621f2ce7d7ed3d139369640e69f862cc8d4f3
Bhavya Kohli <bhavyakohli2024@gmail.com>
2026-03-28 16:21:47 +0800

    [experimental] randlen dataset(s) base. rand len datasets are created differently in the final ver, no need for these classes

--- opas/data.py
+++ opas/data.py
@@ -227,3 +227,122 @@ class PairDatasetTrainHPlaneQCSc(PairDatasetTest):
         # c = np.stack([self.c[l_] for l_ in l])
         
         return q, F.one_hot(torch.tensor(l), num_classes=self.clen).sum(dim=0), self.qcscores[idx]
+
+
+def pad_corpus(c, nmax):
+    if type(c) is np.ndarray:
+        c = torch.from_numpy(c).float()
+
+    pad_setting = [0 for _ in range(2*len(c.shape))]
+    pad_setting[-1] = nmax - c.shape[0]
+    m_a = F.pad(torch.ones(len(c)), (0, nmax - len(c)))
+    c = F.pad(c, pad_setting)
+    return c, m_a
+
+class PairDatasetTrainRandLen(Dataset):
+    def __init__(self, filepath, nmax, num_q=300, negative_exploration=800, seed=15):
+        super().__init__()
+        self.data = h5py.File(filepath, "r")
+
+        np.random.seed(seed)
+        if num_q == -1: 
+            num_q = len(self.data[f"labels"])
+
+        self.qlocs = np.random.choice(range(len(self.data['Q'])), size=num_q, replace=False)
+        self.q = self.data['Q'][:][self.qlocs]
+        self.l = self.data['labels'][:][self.qlocs]
+        self.c = self.data['C'][:]
+
+        self.exploration = []
+        for i in range(len(self.q)):
+            q, l = self.q[i], self.l[i]
+            remaining_idx = list(set(range(len(self.c))) - set(l))
+            exploration = list(l) + np.random.choice(remaining_idx, size=negative_exploration, replace=False).tolist()
+            exploration = np.random.choice(exploration, size=len(exploration), replace=False).tolist()
+            self.exploration.append(exploration)
+        self.exploration = np.stack(self.exploration)
+
+        self.clen = self.exploration.shape[1]
+        self.nmax = nmax
+
+    def get_pair_ids(self, idx):
+        id1 = idx // self.clen
+        id2 = idx - self.clen * (id1)
+        return id1, id2
+
+    def get_dataloader(self, batch_size, shuffle=True, **kwargs):
+        return DataLoader(self, batch_size=batch_size, shuffle=shuffle, num_workers=8, **kwargs)
+
+    def _pad_corpus(self, c):
+        return pad_corpus(c, self.nmax)
+
+    def __getitem__(self, idx):
+        qloc, cloc = self.get_pair_ids(idx)
+        cloc = self.exploration[qloc][cloc]
+        q, c = self.q[qloc], self.c[cloc]
+        l = self.l[qloc]
+
+        label = 1 if cloc in l else 0
+        
+        q, c = torch.from_numpy(q).float(), torch.from_numpy(c).float()
+        c, m_a = self._pad_corpus(c)
+        return q, c, m_a, label
+
+    def get_positive_samples(self, num_samples=1):
+        positive_samples = np.random.randint(len(self.q), size=num_samples)
+        q, l = self.q[positive_samples], self.l[positive_samples]
+        # l has 16 indices, need to pick one
+        l = [np.random.choice(l_, p=[0.1] + [0.9/(len(l_)-1)]*(len(l_)-1)) for l_ in l]
+        label = torch.ones(num_samples)
+
+        c_pl_len = [self._pad_corpus(self.c[l_]) for l_ in l]
+        c = torch.stack([c[0] for c in c_pl_len])
+        m_a = torch.stack([c[1] for c in c_pl_len])
+        q = torch.from_numpy(q).float()
+
+        q, c = torch.atleast_3d(q), torch.atleast_3d(c)
+        return q, c, m_a, label
+
+    def __len__(self):
+        return len(self.q) * self.clen
+    
+
+class PairDatasetTestRandlen(Dataset):
+    def __init__(self, filepath, nmax):
+        super().__init__()
+        self.data = h5py.File(filepath, "r")
+
+        self.q = self.data['Q'][:]
+        self.l = self.data['labels'][:]
+        self.c = self.data['C'][:]
+
+        self.clen = len(self.c)
+        self.lonehot = F.one_hot(torch.from_numpy(self.l), num_classes=self.clen).sum(dim=1)
+
+        self.nmax = nmax
+
+        self.c, self.m_a = self._pad_corpus_full(self.c)
+        self.c = self.c.numpy()
+
+    def get_dataloader(self, batch_size, shuffle=True, **kwargs):
+        return DataLoader(self, batch_size=batch_size, shuffle=shuffle, num_workers=8, **kwargs)
+    
+    def _pad_corpus_full(self, c):
+        if type(c) is np.ndarray:
+            c = torch.from_numpy(c).float()
+
+        pad_setting = [0 for _ in range(2*len(c.shape)-2)]
+        pad_setting[-1] = self.nmax - c.shape[1]
+        pad_setting += [0, 0]
+        m_a = torch.stack([F.pad(torch.ones(len(c_)), (0, self.nmax-len(c_))) for c_ in c])
+        c = F.pad(c, pad_setting)
+        return c, m_a
+
+    def __getitem__(self, idx):
+        q, l = self.q[idx], self.l[idx]
+        q = torch.from_numpy(q).float()
+
+        return q, F.one_hot(torch.tensor(l), num_classes=self.clen).sum(dim=0)
+
+    def __len__(self):
+        return len(self.q)
